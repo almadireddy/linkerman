bf28305d70b6d5ceb032c1fe3b335a9c29b1e4ac
Al Madireddy <al.madireddy@gmail.com>
2024-08-06 21:15:06 -0700

    Add secrets reference and connection to db

--- apps/linkerman/src/app.ts
+++ apps/linkerman/src/app.ts
@@ -1,13 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { Client } from 'pg';
+
+const client = new Client();
+
+(async () => {
+  try {
+    await client.connect();
+    console.log('Connected to postgres :)');
+  } catch (e) {
+    console.log(e);
+  }
+  finally {
+    await client.end();
+  }
+})();
 
 dotenv.config();
 
 const app = express();
 const { PORT } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello there!');
+app.get('/', async (req, res) => {
+  const result = await client.query('select 1 as helloworld'); 
+  res.send(`Hello there! ${result.rowCount ?? 'was null'}`);
 });
 
 app.get('/health', (req, res) => {
